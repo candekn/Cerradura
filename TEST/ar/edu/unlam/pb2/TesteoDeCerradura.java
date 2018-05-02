7d3e6df8c2f519b36f9139c05a5bca588f81c8a3
Candekn <cande_kn@hotmail.com.ar>
2018-05-02 16:16:37 -0300

    Actividad de Cerradura, Alumna Rosario Delgado Programacion 2

---
+++ TEST/ar/edu/unlam/pb2/TesteoDeCerradura.java
@@ -0,0 +1,35 @@
+package ar.edu.unlam.pb2;
+//Rosario Delgado, Programacion 2 UNLaM.
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteoDeCerradura {
+
+	@Test
+	public void testCreacionDeCerradura() {
+		Cerradura cerraduraUno = new Cerradura(27777, 3);
+		cerraduraUno.abrirCerradura(27777);
+		Integer esperado=27777;
+		assertEquals(esperado, cerraduraUno.getContrasenia());	
+	}
+	@Test
+	public void testAbrirCerradura(){
+		Cerradura cerraduraDos = new Cerradura(5410, 2);
+		assertTrue(cerraduraDos.abrirCerradura(5410));
+	}
+	@Test 
+	public void testQueNoAbraCerradura(){
+		Cerradura cerraduraTres = new Cerradura(454, 3);
+		assertFalse(cerraduraTres.abrirCerradura(6998));
+	}
+	@Test
+	public void testCerraduraCerradaPorBloqueo(){
+		Cerradura cerradura4 = new Cerradura(78910,2);
+		cerradura4.abrirCerradura(24520);
+		cerradura4.abrirCerradura(9797);
+		cerradura4.abrirCerradura(78910);
+		assertFalse(cerradura4.abrirCerradura(78910)); //Aunque ponga la clave correcta, no se abre porque está bloqueada.
+	}
+}
---
+++ src/ar/edu/unlam/pb2/Cerradura.java
@@ -0,0 +1,61 @@
+package ar.edu.unlam.pb2;
+//Rosario Delgado, Programacion 2 UNLaM.
+
+public class Cerradura {
+	//Atributos
+	private Integer contrasenia;
+	private Integer intentos;
+	private Boolean estado;
+	//Metodos
+	public Cerradura(Integer contrasenia, Integer intentos){
+		this.contrasenia=contrasenia;
+		if(intentos>0) //esto lo puse para que el numero de intentos ingresado no sea  negativo
+			{
+				this.intentos=intentos;
+			}
+			else
+				{
+					this.intentos=0;
+				}
+		this.estado=false;
+	}
+	public Boolean abrirCerradura(Integer clave){
+		if(this.intentos>0)
+		{
+			if(clave.equals(this.contrasenia)) //Puse equals porque si ponía ==, no me comparaba correctamente más de 2 cifras
+			{
+				this.estado=true;
+				return this.estado;
+			}
+			else
+			{
+				this.intentos=this.intentos-1;
+				return this.estado;
+			}
+		}
+		else
+		{
+			return this.estado;
+		}
+	}
+	public Integer getContrasenia(){
+		return this.contrasenia;
+	}
+	public void setContrasenia(Integer contrasenia){
+		this.contrasenia=contrasenia;
+	}
+	public Integer getIntentos(){
+		return this.intentos;
+	}
+	public void setIntentos(Integer intentos){
+		this.intentos=intentos;
+	}												//No sabia si poner los setters de Intentos
+	public Boolean getEstado(){						//y Estado, porque seria facil abrir una
+		return this.estado;							//cerradura bloqueada desde estos metodos,
+	}												//pero los puse igual. 
+	public void setEstado(Boolean estado){
+		this.estado=estado;
+	}
+	
+	
+}
