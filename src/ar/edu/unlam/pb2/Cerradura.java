2243c56201654d40ea8fa03ed19ce1e4ed84452a
candekn <31010096+candekn@users.noreply.github.com>
2018-05-09 08:42:08 -0300

    Añadido atributo intentos + LimiteDeIntentos

--- src/ar/edu/unlam/pb2/Cerradura.java
+++ src/ar/edu/unlam/pb2/Cerradura.java
@@ -4,32 +4,35 @@ package ar.edu.unlam.pb2;
 public class Cerradura {
 	//Atributos
 	private Integer contrasenia;
-	private Integer intentos;
+	private Integer limiteDeIntentos;
 	private Boolean estado;
+	private Integer intentos;
 	//Metodos
-	public Cerradura(Integer contrasenia, Integer intentos){
+	public Cerradura(Integer contrasenia, Integer limiteDeIntentos){
 		this.contrasenia=contrasenia;
-		if(intentos>0) //esto lo puse para que el numero de intentos ingresado no sea  negativo
+		if(limiteDeIntentos>0) //esto lo puse para que el numero de limiteDeIntentos ingresado no sea  negativo
 			{
-				this.intentos=intentos;
+				this.limiteDeIntentos=limiteDeIntentos;
+				this.intentos=limiteDeIntentos;
 			}
 			else
 				{
-					this.intentos=0;
+					this.limiteDeIntentos=0;
 				}
 		this.estado=false;
 	}
 	public Boolean abrirCerradura(Integer clave){
-		if(this.intentos>0)
+		if(this.limiteDeIntentos>0)
 		{
 			if(clave.equals(this.contrasenia)) //Puse equals porque si ponía ==, no me comparaba correctamente más de 2 cifras
 			{
 				this.estado=true;
+				this.limiteDeIntentos=this.intentos;
 				return this.estado;
 			}
 			else
 			{
-				this.intentos=this.intentos-1;
+				this.limiteDeIntentos=this.limiteDeIntentos-1;
 				return this.estado;
 			}
 		}
@@ -48,18 +51,23 @@ public class Cerradura {
 	public void setContrasenia(Integer contrasenia){
 		this.contrasenia=contrasenia;
 	}
-	public Integer getIntentos(){
-		return this.intentos;
+	public Integer getlimiteDeIntentos(){
+		return this.limiteDeIntentos;
 	}
-	public void setIntentos(Integer intentos){
-		this.intentos=intentos;
-	}												//No sabia si poner los setters de Intentos
+	public void setlimiteDeIntentos(Integer limiteDeIntentos){
+		this.limiteDeIntentos=limiteDeIntentos;
+	}												//No sabia si poner los setters de limiteDeIntentos
 	public Boolean getEstado(){						//y Estado, porque seria facil abrir una
 		return this.estado;							//cerradura bloqueada desde estos metodos,
 	}												//pero los puse igual. 
 	public void setEstado(Boolean estado){
 		this.estado=estado;
 	}
-	
+	public Integer getIntentos() {
+		return this.intentos;
+	}
+	public void setIntentos(Integer intentos) {
+		this.intentos=intentos;;
+	}
 	
 }
